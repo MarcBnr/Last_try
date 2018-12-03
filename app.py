f2eb6bf7b3c87d5c22ed0f81256703c7d49deb93
marcbnr <33203155+MarcBnr@users.noreply.github.com>
2018-12-03 16:35:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-
-# coding: utf-8
-
-# In[8]:
-
-
 # Final Project
 # Create a Dashboard taking data from [Eurostat, GDP and main components (output, expenditure and income)](http://ec.europa.eu/eurostat/web/products-datasets/-/nama_10_gdp). 
 # The dashboard will have two graphs: 
 # * The first one will be a scatterplot with two DropDown boxes for the different indicators. It will have also a slide for the different years in the data. 
 # * The other graph will be a line chart with two DropDown boxes, one for the country and the other for selecting one of the indicators. (hint use Scatter object using mode = 'lines' [(more here)](https://plot.ly/python/line-charts/) 
-
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
@@ -56,5 +49,98 @@ app.layout = html.Div([
         value=eurostat['TIME'].max(),
         step=None,
         marks={str(time): str(time) for time in eurostat['TIME'].unique()},
-    ), style={'marginRight': 50, 'marginLeft': 110})])
-
+    ), style={'marginRight': 50, 'marginLeft': 110},),
+#Second chart
+# Second graph name is id graph2
+    
+    html.Div([
+        
+        html.Div([
+            dcc.Dropdown( 
+                id='xaxis-column2',
+                options=[{'label': i, 'value': i} for i in available_indicators],
+                value='Gross domestic product at market prices'
+            )
+        ],
+        style={'width': '30%', 'marginTop': 40, 'display': 'inline-block'}),
+        html.Div([
+            dcc.Dropdown( 
+                id='yaxis-column2',
+                options=[{'label': i, 'value': i} for i in available_countries],
+                value= "Spain"    
+            )
+        ],style={'width': '30%', 'marginTop': 40, 'float': 'right', 'display': 'inline-block'})
+     ]),
+     dcc.Graph(id='graph2'),
+])
+#This is the call back function for the first graph
+@app.callback(
+    dash.dependencies.Output('graph1', 'figure'),
+    [dash.dependencies.Input('xaxis-column1', 'value'),
+     dash.dependencies.Input('yaxis-column1', 'value'),
+     dash.dependencies.Input('year--slider', 'value')])
+#Dataframe for the time
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 year_value):
+    
+    eurostatframe = eurostat[eurostat['TIME'] == year_value]
+    return {
+        'data': [go.Scatter(
+            x=eurostatframe[eurostatframe['NA_ITEM'] == xaxis_column_name]['Value'],
+            y=eurostatframe[eurostatframe['NA_ITEM'] == yaxis_column_name]['Value'],
+            text=eurostatframe[eurostatframe['NA_ITEM'] == yaxis_column_name]['GEO'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear'
+            },
+            margin={'l': 110, 'b': 50, 't': 20, 'r': 50},
+            hovermode='closest'
+        )
+    }
+#This is the call back function for the second chart
+@app.callback(
+    dash.dependencies.Output('graph2', 'figure'),
+    [dash.dependencies.Input('xaxis-column2', 'value'),
+     dash.dependencies.Input('yaxis-column2', 'value')])
+#As here I have all of the years I just have to update the column names of the chart
+def update_graph(xaxis_column_name, yaxis_column_name):
+    
+    eurostatframe = eurostat_1[eurostat_1['GEO'] == yaxis_column_name]
+    return {
+        'data': [go.Scatter(
+            x=eurostatframe['TIME'].unique(),
+            y=eurostatframe[eurostatframe['NA_ITEM'] == xaxis_column_name]['Value'],
+            mode='lines',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={
+                'title': xaxis_column_name,
+                'type': 'linear'
+            },
+            yaxis={
+                'title': yaxis_column_name,
+                'type': 'linear'
+            },
+            margin={'l': 110, 'b': 50, 't': 20, 'r': 50},
+            hovermode='closest'
+        )
+    }
+if __name__ == '__main__':
+    app.run_server()
